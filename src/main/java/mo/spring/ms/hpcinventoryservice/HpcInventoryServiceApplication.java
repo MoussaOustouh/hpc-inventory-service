f3dd3d546212ea2c9c36db68e1270685c029b674
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 01:47:32 +0100

    add properties to application.properties file

--- src/main/java/mo/spring/ms/hpcinventoryservice/HpcInventoryServiceApplication.java
+++ src/main/java/mo/spring/ms/hpcinventoryservice/HpcInventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package mo.spring.ms.hpcinventoryservice;
 
+import mo.spring.ms.hpcinventoryservice.entities.Product;
+import mo.spring.ms.hpcinventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HpcInventoryServiceApplication {
@@ -10,4 +14,14 @@ public class HpcInventoryServiceApplication {
         SpringApplication.run(HpcInventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(new Product(null, "Computer", 1000, 30));
+            productRepository.save(new Product(null, "Monitor", 800, 20));
+            productRepository.save(new Product(null, "Phone", 500, 50));
+
+            productRepository.findAll().forEach(p -> System.out.println(p.toString()));
+        };
+    }
 }
---
+++ src/main/java/mo/spring/ms/hpcinventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package mo.spring.ms.hpcinventoryservice.repositories;
+
+import mo.spring.ms.hpcinventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
