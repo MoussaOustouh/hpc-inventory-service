7fd4b9916474b6854324bd9243818eba386fcce9
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-16 01:29:17 +0100

    first commit

---
+++ src/main/java/mo/spring/ms/hpcinventoryservice/HpcInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.ms.hpcinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mo/spring/ms/hpcinventoryservice/HpcInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.spring.ms.hpcinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
